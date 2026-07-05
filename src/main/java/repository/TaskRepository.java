22641a854ce9f46003fc7f00b98192636b3043ed
Emac56 <emacvicente8@gmail.com>
2026-07-05 21:16:40 +0800

    Create repository Task Create method loadTask Create method saveTask Create method saveAllTask Create method updateTask

---
+++ src/main/java/repository/TaskRepository.java
@@ -0,0 +1,88 @@
+package repository;
+
+import model.Task;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+
+    private File databaseFile;
+
+    public TaskRepository(File databaseFile) {
+        this.databaseFile = databaseFile;
+    }
+    public List<Task> loadTask() throws IOException {
+        List<Task> taskList = new ArrayList<>();
+
+        var reader = new BufferedReader(new FileReader(databaseFile));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+
+            String[] data = line.split("\\|");
+
+            Task task = new Task (
+                    Long.parseLong(data[0]),
+                    data[1],
+                    data[2],
+                    data[3],
+                    data[4],
+                    Integer.parseInt(data[5]),
+                    data[6]
+            );
+            taskList.add(task);
+        }
+        reader.close();
+        return taskList;
+    }
+    public void saveTask(Task task) throws IOException {
+        var writer = new BufferedWriter(new FileWriter(databaseFile, true));
+
+        writer.write(
+                task.getTaskId() + "|" +
+                    task.getTaskTitle() + "|" +
+                    task.getSubject() + "|" +
+                    task.getTaskType() + "|" +
+                    task.getDueDate() + "|" +
+                    task.getEstimatedHours() + "|" +
+                    task.getStatus()
+        );
+        writer.newLine();
+        writer.close();
+    }
+
+    public void saveAllTask(List<Task> taskList) throws IOException {
+        var writer = new BufferedWriter(new FileWriter(databaseFile));
+
+        for (Task task : taskList) {
+            writer.write(
+                    task.getTaskId() + "|" +
+                            task.getTaskTitle() + "|" +
+                            task.getSubject() + "|" +
+                            task.getTaskType() + "|" +
+                            task.getDueDate() + "|" +
+                            task.getEstimatedHours() + "|" +
+                            task.getStatus()
+            );
+            writer.newLine();
+            writer.close();
+        }
+    }
+    public void updateTask(Task updatedTask) throws IOException {
+
+        List<Task> taskList = loadTask();
+
+        for (int i = 0; i < taskList.size(); i++) {
+
+            Task task = taskList.get(i);
+
+            if (task.getTaskId().equals(updatedTask.getTaskId())) {
+                taskList.set(i, updatedTask);
+                break;
+            }
+        } saveAllTask(taskList);
+    }
+}
