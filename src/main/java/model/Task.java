e85c25d18b55c21752e4a7a35801f1cc0a09bb3d
Emac56 <emacvicente8@gmail.com>
2026-07-05 20:23:18 +0800

    Create task model in StudentTaskManager

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,73 @@
+package model;
+
+public class Task {
+    private Long taskId;
+    private String taskTitle,subject,taskType,dueDate,status;
+    private int estimatedHours;
+
+    public Task
+            (Long taskId,String taskTitle,String subject,
+             String taskType,String dueDate,int estimatedHours,
+             String status) {
+        this.taskId = taskId;
+        this.taskTitle = taskTitle;
+        this.subject = subject;
+        this.taskType = taskType;
+        this.dueDate = dueDate;
+        this.estimatedHours = estimatedHours;
+        this.status = status;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public int getEstimatedHours() {
+        return estimatedHours;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setEstimatedHours(int estimatedHours) {
+        this.estimatedHours = estimatedHours;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+}
